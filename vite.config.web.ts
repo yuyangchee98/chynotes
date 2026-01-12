7f8cca597adb42df4ee31495d1a98fd89eec70ca
Yang <yuyangchee98@gmail.com>
2026-01-12 20:42:26 +0900

    config

--- vite.config.web.ts
+++ vite.config.web.ts
@@ -1,9 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import fs from 'fs'
+
+// Plugin to serve index-web.html as the default page
+function serveWebHtml(): Plugin {
+  return {
+    name: 'serve-web-html',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/' || req.url === '/index.html') {
+          const webHtmlPath = path.resolve(__dirname, 'index-web.html')
+          const html = fs.readFileSync(webHtmlPath, 'utf-8')
+          server.transformIndexHtml('/', html).then((transformed) => {
+            res.setHeader('Content-Type', 'text/html')
+            res.end(transformed)
+          })
+          return
+        }
+        next()
+      })
+    },
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), serveWebHtml()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -12,7 +34,7 @@ export default defineConfig({
   base: '/',
   server: {
     port: 5189,
-    host: true, // Allow access from other devices
+    host: true,
   },
   build: {
     outDir: 'dist-web',
